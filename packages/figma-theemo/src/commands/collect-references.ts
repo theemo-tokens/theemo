f72a835a83e4e4e7c1ae6f16bd0d6983998f6579
gossi <gossi@users.noreply.github.com>
2020-01-11 23:25:32 +0100

    Filter export before sending

--- packages/figma-theemo/src/commands/collect-references.ts
+++ packages/figma-theemo/src/commands/collect-references.ts
@@ -1,19 +1,47 @@
 import Command from './command';
 import ReferencesManager from '../manager/references-manager';
 import NodeManager from '../manager/node-manager';
+import { ALL_STYLES } from '../styles/types';
 
 export default class CollectReferencesCommand extends Command {
   NAME = 'collect-references';
 
   execute() {
     const manager = new ReferencesManager();
-    const data = [];
+    const nodes = [];
 
     manager.each((node) => {
       const handler = new NodeManager(node);
-      data.push({ node: node.id, ...handler.data.styles });
+      nodes.push({
+        node: {
+          id: node.id
+        }, ...this.filter(handler.data.styles)
+      });
     });
 
+    const data = {
+      document: {
+        id: figma.root.id,
+        name: figma.root.name
+      },
+      nodes
+    };
+
     this.emitter.sendEvent('references-collected', data);
+  }
+
+  filter(styles) {
+    const filtered = {};
+
+    for (const style of ALL_STYLES) {
+      if (styles[style]) {
+        if (styles[style].from && styles[style].to) {
+          filtered[style] = styles[style];
+          delete filtered[style].local;
+        }
+      }
+    }
+
+    return filtered;
   }
 }
