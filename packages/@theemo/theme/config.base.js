fd09863f59b5ceee78760743175b7ad512042edd
gossi <gossi@users.noreply.github.com>
2022-01-19 19:33:34 +0100

    more sd configs

---
+++ packages/@theemo/theme/config.base.js
@@ -0,0 +1,36 @@
+const modes = ['light', 'dark'];
+
+module.exports = {
+  source: [
+    // this is saying find any files in the tokens folder
+    // that does not have .dark or .light, but ends in .json
+    `tokens/**/!(*.${modes.join(`|*.`)}|*.transient).json`
+  ],
+  include: ['tokens/**/*.transient.json'],
+  platforms: {
+    web: {
+      transforms: [
+        'name/cti/kebab',
+        'time/seconds',
+        'content/icon',
+        'size/rem',
+        'color/css'
+      ],
+      buildPath: 'build/',
+      files: [
+        {
+          format: 'css/variables',
+          destination: 'base.css',
+          options: {
+            outputReferences: true,
+            showFileHeader: false
+          },
+          filter(token) {
+            return !token.colorScheme && token.transient !== true;
+          }
+        }
+      ]
+    }
+  },
+  modes
+};
