5234bf0604f9ca7586346be04b34bd4dc1a7a1e0
gossi <gossi@users.noreply.github.com>
2023-09-21 16:54:48 +0200

    Revert "Release v0.1.0"
    This reverts commit 9baa7b79b355e6a74e19fd919bf40d6c0475104e.

--- .release-it.js
+++ .release-it.js
@@ -15,7 +15,7 @@ module.exports = {
   },
   git: {
     tagName: 'v${version}',
-    commitMessage: 'Release v${version}',
+    commitMessage: 'release v${version}',
     requireCleanWorkingDir: false,
     changelog: false
   },
