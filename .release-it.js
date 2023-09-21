12210e31a6e5dc32404aa3e0dfaa6b638fc62b58
gossi <gossi@users.noreply.github.com>
2023-09-21 16:15:44 +0200

    Extract release-it config

---
+++ .release-it.js
@@ -0,0 +1,29 @@
+module.exports = {
+  plugins: {
+    '@release-it-plugins/workspaces': {
+      workspaces: [
+        // glob still finds private repositories o_O
+        // 'packages/@theemo/*',
+        'packages/@theemo/build',
+        'packages/@theemo/cli',
+        'packages/@theemo/figma',
+        'packages/@theemo/style-dictionary',
+        'packages/@theemo/sync',
+        'packages/@theemo/tokens'
+      ]
+    }
+  },
+  git: {
+    tagName: 'v${version}',
+    commitMessage: 'release v${version}',
+    requireCleanWorkingDir: false,
+    changelog: false
+  },
+  github: {
+    release: true,
+    tokenRef: "GITHUB_AUTH"
+  },
+  npm: {
+    publish: true
+  }
+}
