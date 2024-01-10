87efe1c2b7c72a4e2b4051aa8ec38eccb87d9e9c
Thomas Gossmann <gossi@users.noreply.github.com>
2024-01-10 15:33:43 +0100

    Remove Outdated Stuff (#727)

--- packages/theemo/src/tools/config.ts
+++
@@ -1,7 +0,0 @@
-import type ReaderConfig from '../sync/reader/config.js';
-import type WriterConfig from '../sync/writer/config.js';
-
-export default interface Config {
-  reader?: ReaderConfig;
-  writer?: WriterConfig;
-}
--- website/README.md
+++
@@ -1,7 +0,0 @@
-# Theemo Website
-
-## Installation
-
-- `yarn install` in this folder
-- In this directory, do `yarn start`.
-- A browser window will open up, pointing to the docs.
