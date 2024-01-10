1c17befbdd0563ea1a80685defdb95c6725c96c4
Thomas Gossmann <gossi@users.noreply.github.com>
2024-01-10 15:41:01 +0100

    Publish W3C Token Parser (#729)

--- packages/@theemo/style-dictionary/src/index.ts
+++ packages/@theemo/style-dictionary/src/index.ts
@@ -1,3 +1,4 @@
 export type { StyleDictionaryWriterConfig } from './config';
 export * from './extend';
+export { w3cTokenJsonParser } from './parsers/w3c-token-json-parser';
 export { styleDictionaryWriter } from './tool';
