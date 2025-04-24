e82f247de299f60d3e5b6cf9734af8449f7e48d8
Thomas Gossmann <gossi@users.noreply.github.com>
2025-04-25 00:29:29 +0200

    Add support for declaration maps (#1310)

--- configs/tsdown/src/index.js
+++ configs/tsdown/src/index.js
@@ -1,10 +1,14 @@
 import { defineConfig } from 'tsdown';
+import IsolatedDecl from 'unplugin-isolated-decl/rolldown';
 
 export default defineConfig({
   entry: ['src/index.ts'],
   splitting: false,
-  sourcemap: true,
   clean: true,
   format: ['cjs', 'esm'],
-  dts: true
+  plugins: [
+    IsolatedDecl({
+      sourceMap: true
+    })
+  ]
 });
