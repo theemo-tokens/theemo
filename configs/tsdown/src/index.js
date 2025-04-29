2f046b9c6c054a35ba707e2a2379c9a9c15e5395
gossi <gossi@users.noreply.github.com>
2025-04-29 18:41:22 +0200

    Revert "Add support for declaration maps (#1310)"
    This reverts commit e82f247de299f60d3e5b6cf9734af8449f7e48d8.

--- configs/tsdown/src/index.js
+++ configs/tsdown/src/index.js
@@ -1,14 +1,10 @@
 import { defineConfig } from 'tsdown';
-import IsolatedDecl from 'unplugin-isolated-decl/rolldown';
 
 export default defineConfig({
   entry: ['src/index.ts'],
   splitting: false,
+  sourcemap: true,
   clean: true,
-  format: 'esm',
-  plugins: [
-    IsolatedDecl({
-      sourceMap: true
-    })
-  ]
+  format: ['cjs', 'esm'],
+  dts: true
 });
