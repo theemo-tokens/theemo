64bd728c6e0411f003514f0b4d995f92bc720d99
gossi <gossi@users.noreply.github.com>
2025-05-10 14:38:25 +0200

    Move packages

--- vitest.config.js
+++ vitest.config.js
@@ -3,16 +3,16 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     workspace: [
-      'packages/@theemo/figma',
-      'packages/@theemo/tokens',
-      'packages/@theemo/style-dictionary',
-      'packages/@theemo/theme/vitest.config.node.ts'
+      'packages/figma',
+      'packages/tokens',
+      'packages/style-dictionary',
+      'packages/theme/vitest.config.node.ts'
     ],
     coverage: {
       enabled: true,
       provider: 'v8',
       reporter: ['html', 'clover'],
-      include: ['packages/@theemo/*/src/**'],
+      include: ['packages/*/src/**'],
       all: true
     }
   }
