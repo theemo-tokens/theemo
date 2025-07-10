9b0734b124c4fa3718b3f0fa84d687dd2f05510b
gossi <gossi@users.noreply.github.com>
2025-07-10 16:18:55 +0200

    Attempt to write tests emulating the browser

--- playground/vanilla-js/tests/manager.test.ts
+++ playground/vanilla-js/tests/manager.test.ts
@@ -1,10 +1,13 @@
 import { Principal, ThemeManager } from '@theemo/theme';
-import { describe, expect, test } from 'vitest';
+import { describe, expect, test, vi } from 'vitest';
+// import { server } from '@vitest/browser/context'
+// const { canEmulateColorScheme, emulateColorScheme } = server.commands
 
-describe('ThemeManager', () => {
+describe('ThemeManager', async () => {
+  // const emulationPossible = await canEmulateColorScheme();
   const manager = new ThemeManager();
 
-  const findThemeByName = (name: string) => {
+  const findFeatureByName = (name: string) => {
     return manager.features.find((f) => f.name === name);
   };
 
@@ -21,21 +24,21 @@ describe('ThemeManager', () => {
   });
 
   test('overwrite browser feature', () => {
-    let colorSchemeFeature = findThemeByName('color-scheme');
+    let colorSchemeFeature = findFeatureByName('color-scheme');
 
     expect(colorSchemeFeature?.principal).toBe(Principal.Browser);
 
     // overwrite
     manager.setFeature('color-scheme', 'dark');
 
-    colorSchemeFeature = findThemeByName('color-scheme');
+    colorSchemeFeature = findFeatureByName('color-scheme');
 
     expect(colorSchemeFeature?.principal).toBe(Principal.User);
 
     // unset
     manager.unsetFeature('color-scheme');
 
-    colorSchemeFeature = findThemeByName('color-scheme');
+    colorSchemeFeature = findFeatureByName('color-scheme');
 
     expect(colorSchemeFeature?.principal).toBe(Principal.Browser);
   });
@@ -63,4 +66,23 @@ describe('ThemeManager', () => {
       async () => await manager.switchTheme('theme-that-does-not-exist')
     ).rejects.toThrowError(`Cannot switch theme 'theme-that-does-not-exist': theme doesn't exist`);
   });
+
+  // test.runIf(emulationPossible)('browser to switch color scheme and manager to react appropriately', async () => {
+  //   await manager.switchTheme('ocean');
+  //   await emulateColorScheme('dark');
+
+  //   let colorSchemeFeature = findFeatureByName('color-scheme');
+
+  //   expect(colorSchemeFeature?.value).toBe('dark');
+
+  //   colorSchemeFeature = findFeatureByName('color-scheme');
+
+  //   expect(colorSchemeFeature?.value).toBe('light');
+
+  //   await emulateColorScheme('dark');
+
+  //   colorSchemeFeature = findFeatureByName('color-scheme');
+
+  //   expect(colorSchemeFeature?.value).toBe('dark');
+  // })
 });
